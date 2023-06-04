2ce1462d0788dad8876cf600fb56a8830e2b7b59
Honore Hounwanou <mercuryseries@gmail.com>
2023-06-04 12:33:47 -0400

    wip form submission handling

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -10,14 +11,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MessagesController extends AbstractController
 {
-    #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function create(): Response
+    #[Route('/', name: 'app_home', methods: ['GET', 'POST'])]
+    public function create(Request $request): Response
     {
         $form = $this->createFormBuilder()
             ->add('email', EmailType::class)
             ->add('body', TextareaType::class)
             ->getForm()
         ;
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // TODO: Save message in database
+            // TODO: Send email
+            // TODO: Redirect back with success message
+            
+            dd('form handling');
+        }
 
         return $this->render('messages/create.html.twig', compact('form'));
     }
