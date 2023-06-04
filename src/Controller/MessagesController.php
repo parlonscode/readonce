72f5ce7b522de7882aeec2213d45a0537fd42842
Honore Hounwanou <mercuryseries@gmail.com>
2023-06-04 15:48:09 -0400

    Add app_messages_show route

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -8,6 +8,7 @@ use App\Repository\MessageRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MessagesController extends AbstractController
@@ -30,5 +31,18 @@ class MessagesController extends AbstractController
         }
 
         return $this->render('messages/create.html.twig', compact('form'));
+    }
+
+    #[Route(
+        '/messages/{id}',
+        requirements: [
+            'id' => Requirement::POSITIVE_INT
+        ],
+        name: 'app_messages_show',
+        methods: ['GET']
+    )]
+    public function show(Message $message): Response
+    {
+        dd($message);
     }
 }
