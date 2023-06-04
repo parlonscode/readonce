d6f14dbb0f32abde2e176dab1f0ca824919ecc75
Honore Hounwanou <mercuryseries@gmail.com>
2023-06-04 12:25:53 -0400

    Install symfony/form and add message creation form

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MessagesController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
     public function create(): Response
     {
-        return $this->render('messages/create.html.twig');
+        $form = $this->createFormBuilder()
+            ->add('email', EmailType::class)
+            ->add('body', TextareaType::class)
+            ->getForm()
+        ;
+
+        return $this->render('messages/create.html.twig', compact('form'));
     }
 }
