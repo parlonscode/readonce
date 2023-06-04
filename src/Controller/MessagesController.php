3cd8de62f76dae60d3f574235b58bbd88a02c898
Honore Hounwanou <mercuryseries@gmail.com>
2023-06-04 15:33:33 -0400

    Create MessageType form class

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -3,12 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Message;
+use App\Form\MessageType;
 use App\Repository\MessageRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MessagesController extends AbstractController
@@ -18,11 +17,7 @@ class MessagesController extends AbstractController
     {
         $message = new Message;
 
-        $form = $this->createFormBuilder($message)
-            ->add('email', EmailType::class)
-            ->add('body', TextareaType::class)
-            ->getForm()
-        ;
+        $form = $this->createForm(MessageType::class, $message);
 
         $form->handleRequest($request);
 
