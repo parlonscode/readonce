beec07c8d15c339fd2101f00cac65b88ff51d94f
Honore Hounwanou <mercuryseries@gmail.com>
2023-06-04 15:23:58 -0400

    Add server-side validation for message creation form

--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -6,6 +6,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use App\Entity\Trait\Timestampable;
 use App\Repository\MessageRepository;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: MessageRepository::class)]
 #[ORM\Table(name: 'messages')]
@@ -20,9 +21,12 @@ class Message
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Email]
     private ?string $email = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank]
     private ?string $body = null;
 
     public function getId(): ?int
