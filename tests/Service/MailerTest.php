421d9f305a67f27becf4904bdcdd2b2315186410
Honore Hounwanou <mercuryseries@gmail.com>
2023-06-05 13:22:02 -0400

    WIP

---
+++ tests/Service/MailerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Entity\Message;
+use App\Service\Mailer;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+
+class MailerTest extends TestCase
+{
+    private $symfonyMailer;
+    private $mailer;
+
+    /** @test **/
+    public function sendReadOnceMessage_should_work_properly(): void
+    {
+        $symfonyMailer = $this->createMock(MailerInterface::class);
+        $mailer = new Mailer($symfonyMailer);
+
+        $message = new Message;
+        $message->setEmail('johndoe@example.com');
+        $message->setBody('Hello from the other side!');
+
+        $symfonyMailer->expects($this->once())
+            ->method('send');
+
+        $mailer->sendReadOnceMessage($message);
+    }
+}
